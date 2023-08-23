47edbb1b07ecb475bd74c42e5d54d997fea00d18
xyw3kLsi <solution.lustek@outlook.com>
2023-08-23 01:24:11 -0400

    day 3 code push

---
+++ src/main/java/com/automation/coding/Pnt_August_2023_WeekDay_Batch/App.java
@@ -0,0 +1,13 @@
+package com.automation.coding.Pnt_August_2023_WeekDay_Batch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/automation/coding/Pnt_August_2023_WeekDay_Batch/AppTest.java
@@ -0,0 +1,20 @@
+package com.automation.coding.Pnt_August_2023_WeekDay_Batch;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
